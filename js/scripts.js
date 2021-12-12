f02a2d5c2f5cdcb8efc5370778cc0db660355cda
Sharon-Korir <sharon.korir@student.moringaschool.com>
2021-12-13 01:22:19 +0300

    update functions

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,23 @@ function submitForm (event){
   console.log(birthday);
   if(validateDate(birthday)){
     document.getElementById("userMessage").hidden = true;
-    console.log(getDayOfBirth(birthday))
+    var dayOfBirth = getDayOfBirth(birthday);
+    console.log(dayOfBirth)
+    var male = document.getElementById("male").checked;
+    var female = document.getElementById("female").checked;
+    if(male){
+      console.log('male selected', dayOfBirth);
+      var maleName = generateMaleName(dayOfBirth)
+      console.log(maleName)
+    } else if(female){
+      console.log('female selected', dayOfBirth)
+      var femaleName = generateFemaleName(dayOfBirth)
+      console.log(femaleName)
+    } else{
+      console.log('none selected')
+      document.getElementById("userMessage").hidden = false;
+      document.getElementById("userMessage").innerText = "gender not set.";
+    }
   } else {
     document.getElementById("userMessage").hidden = false;
     document.getElementById("userMessage").innerText = "birthday not set.";
@@ -23,12 +39,89 @@ function validateDate (date){
   }
 }
 
-function validateGender (){
-
-}
 function getDayOfBirth (date){
   var d = new Date(date);
   var day = d.getDay();
   var weekDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
   return weekDay[day]
+}
+
+function generateMaleName(day) {
+  console.log(day)
+  var maleNames = [
+    {
+      name: 'Kwasi',
+      day: 'Sunday'
+    },
+    {
+      name: 'Kwadwo',
+      day: 'Monday'
+    },
+    {
+      name: 'Kwabena',
+      day: 'Tuesday'
+    },
+    {
+      name: 'Kwaku',
+      day: 'Wednesday'
+    },
+    {
+      name: 'Yaw',
+      day: 'Thursday'
+    },
+    {
+      name: 'Kofi',
+      day: 'Friday'
+    },
+    {
+      name: 'Kwame',
+      day: 'Saturday'
+    }
+  ]
+  console.log(maleNames)
+  maleNames.forEach(function (object) {
+    if(object.day === day){
+      return object.name;
+    }
+  })
+}
+
+function generateFemaleName(day) {
+  console.log(day)
+  var femaleNames = [
+    {
+      name: 'Akosua',
+      day: 'Sunday'
+    },
+    {
+      name: 'Adwoa',
+      day: 'Monday'
+    },
+    {
+      name: 'Abenaa',
+      day: 'Tuesday'
+    },
+    {
+      name: 'Akua',
+      day: 'Wednesday'
+    },
+    {
+      name: 'Yaa',
+      day: 'Thursday'
+    },
+    {
+      name: 'Afua',
+      day: 'Friday'
+    },
+    {
+      name: 'Ama',
+      day: 'Saturday'
+    }
+  ]
+  console.log(femaleNames)
+  femaleNames.forEach(function (object) {
+    if(object.day === day){
+      return object.name;
+    }
+  })
 }
