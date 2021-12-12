8013f5d848b06dd4bc6387860fa569ee3c8c26dc
Sharon-Korir <sharon.korir@student.moringaschool.com>
2021-12-13 00:25:58 +0300

    add js control flows

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,34 @@
 function submitForm (event){
-  event.preventDefault()
-  console.log("test")
+  event.preventDefault();
+  console.log("test");
   var birthday = document.getElementById("birthday").value;
-  console.log(birthday)
+  console.log(birthday);
+  if(validateDate(birthday)){
+    document.getElementById("userMessage").hidden = true;
+    console.log(getDayOfBirth(birthday))
+  } else {
+    document.getElementById("userMessage").hidden = false;
+    document.getElementById("userMessage").innerText = "birthday not set.";
+  }
+}
+
+function validateDate (date){
+  if(date === "" || date === " "){
+    console.log("input invalid")
+    return false;
+  } else {
+    //if valid
+    console.log("input valid")
+    return true;
+  }
+}
+
+function validateGender (){
+
+}
+function getDayOfBirth (date){
+  var d = new Date(date);
+  var day = d.getDay();
+  var weekDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  return weekDay[day]
 }
